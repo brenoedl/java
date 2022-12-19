32b065973102df7ca81e741f4fbeec8316f9f99e
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-12-19 18:38:33 -0300

    Hora Do Sistema em java

---
+++ HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -0,0 +1,9 @@
+package com.mycompany.horadosistema;
+import java.util.Date
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A horaa atual é");
+        System.out.println(relogio.toString());
+    }
+}
