237bcd174d59f633a50dda3fbf8a613d630d3b30
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-05 11:12:00 -0300

    DataHoraSistema

---
+++ DataHoraSistema/src/datahorasistema/DataHoraSistema.java
@@ -0,0 +1,12 @@
+package datahorasistema;
+
+import java.time.LocalDateTime;
+
+public class DataHoraSistema {
+
+    public static void main(String[] args) {
+        LocalDateTime dataHora = LocalDateTime.now();
+        System.out.println("A data e a hora do sistema são " + dataHora);
+    }
+    
+}
