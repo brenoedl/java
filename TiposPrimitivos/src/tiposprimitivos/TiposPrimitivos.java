b2689c0f99f6fbd1823646d259086012877ef978
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-08 21:46:08 -0300

    TiposPrimitivos

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package tiposprimitivos;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+       float nota = 8.9f;
+       int idade = 21;
+       String nome = (String) "Breno";
+       boolean namoro = false;  
+        System.out.println("A noa e " + nota);
+        System.out.println("A idade e " + idade);
+        System.out.println("O nome e " + nome);
+        System.out.println(namoro);
+    }
+    
+}
