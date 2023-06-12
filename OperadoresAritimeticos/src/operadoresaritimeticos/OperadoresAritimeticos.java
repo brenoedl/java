fe28f537ad20e1f0711165ea21d7098dd80ef31b
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-11 21:09:10 -0300

    Operadores Aritimeticos

---
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,12 @@
+package operadoresaritimeticos;
+
+public class OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 6;
+        float media = (num1 + num2)/2;
+        System.out.printf("A media entre %d e %d e %f \n", num1, num2, media);
+    }
+    
+}
