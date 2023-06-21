ffdb1fa16cff691cebfa4acfbe44cc14fa680069
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-21 08:35:16 -0300

    Operadores Ternarios

---
+++ OperadoresTernarios/src/operadoresternarios/OperadoresTernarios.java
@@ -0,0 +1,12 @@
+package operadoresternarios;
+
+public class OperadoresTernarios {
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 7;
+        int maior = (n1>n2)?n1:n2;
+        System.out.printf("O maior entre %d e %d é %d\n", n1, n2, maior);
+    }
+    
+}
