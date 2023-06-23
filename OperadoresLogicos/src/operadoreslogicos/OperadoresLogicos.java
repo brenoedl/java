147aa0aa38219b1acfc10828bc41031f50ed0ddf
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-23 15:20:09 -0300

    Operadors Relacionais

---
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,23 @@
+package operadoreslogicos;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        int n1 = 6;
+        int n2 = 5;
+        boolean r1, r2,r3, r4, r5, r6;
+        r1 = n1 > n2;
+        r2 = n1 < n2;
+        r3 = n1 >= n2;
+        r4 = n1 <= n2;
+        r5 = n1 == n2;
+        r6 = n1 != n2;
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r6);
+    }
+    
+}
