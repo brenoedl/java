100a7630036ff9219acc64a06d60f35401b8e9ab
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-26 11:02:34 -0300

    Update OperadoresLogicos.java

--- OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -3,21 +3,17 @@ package operadoreslogicos;
 public class OperadoresLogicos {
 
     public static void main(String[] args) {
-        int n1 = 6;
-        int n2 = 5;
-        boolean r1, r2,r3, r4, r5, r6;
-        r1 = n1 > n2;
-        r2 = n1 < n2;
-        r3 = n1 >= n2;
-        r4 = n1 <= n2;
-        r5 = n1 == n2;
-        r6 = n1 != n2;
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r5);
-        System.out.println(r6);
+        int n1 = 3;
+        int n2 = 6;
+        boolean e, ou, nao, ou_s;
+        e = n2 % 2 == 0 && n1 % 3 == 0;
+        System.out.println(e);
+        ou = n2 % 2 == 0 || n1 % 3 == 1;
+        System.out.println(ou);
+        ou_s = n2 % 2 == 0 ^ n1 % 3 == 0;
+        System.out.println(ou_s);
+        nao = !e;
+        System.out.println(nao);
     }
     
 }
