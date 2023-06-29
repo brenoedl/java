c257952af787c857114c93f09bbb0c1df72d3183
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-29 13:09:27 -0300

    Jogo Adivinhar Numero

---
+++ JogoAdivinhaNumero/src/jogonumero/TelaJogoAdivinhaNumero.java
@@ -0,0 +1,111 @@
+package jogonumero;
+
+import java.util.Random;
+
+
+public class TelaJogoAdivinhaNumero extends javax.swing.JFrame {
+    Random aleatorio = new Random();
+    int valor = aleatorio.nextInt(5)+1;
+    public TelaJogoAdivinhaNumero() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblMemsagem = new javax.swing.JLabel();
+        btnPalpite = new javax.swing.JButton();
+        spmPalpite = new javax.swing.JSpinner();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Jogo de Adivinhar Numero");
+        setResizable(false);
+
+        lblMemsagem.setFont(new java.awt.Font("Arial Black", 1, 18)); // NOI18N
+        lblMemsagem.setForeground(new java.awt.Color(0, 0, 255));
+        lblMemsagem.setText("<html>Vou sortear  um número entre 1 e 5 tente adivinhar!</html>");
+
+        btnPalpite.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnPalpite.setText("palpite");
+        btnPalpite.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPalpiteActionPerformed(evt);
+            }
+        });
+
+        spmPalpite.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        spmPalpite.setModel(new javax.swing.SpinnerNumberModel(1, 1, 5, 1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addComponent(lblMemsagem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(239, 239, 239)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnPalpite)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(spmPalpite, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(lblMemsagem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addComponent(spmPalpite, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
+                .addComponent(btnPalpite)
+                .addContainerGap(23, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPalpiteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPalpiteActionPerformed
+        int palpite = Integer.parseInt(spmPalpite.getValue().toString());
+        String res = (valor == palpite)?"Acertou miserável":"Errou eu sortiei o número "+valor;
+        lblMemsagem.setText(res);
+        valor = aleatorio.nextInt(5)+1;
+    }//GEN-LAST:event_btnPalpiteActionPerformed
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaJogoAdivinhaNumero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaJogoAdivinhaNumero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaJogoAdivinhaNumero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaJogoAdivinhaNumero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaJogoAdivinhaNumero().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnPalpite;
+    private javax.swing.JLabel lblMemsagem;
+    private javax.swing.JSpinner spmPalpite;
+    // End of variables declaration//GEN-END:variables
+}
