a244ff362ca531ab42926764e4f2b328e2a07684
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-30 17:11:14 -0300

    Condicional Composta

---
+++ CondicionalComposta/src/condicionalcomposta/CondicionalComposta.java
@@ -0,0 +1,20 @@
+package condicionalcomposta;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class CondicionalComposta {
+
+    public static void main(String[] args) {
+        LocalDateTime data = LocalDateTime.now();
+        DateTimeFormatter  formato = DateTimeFormatter.ofPattern("yyyy");
+        int anoAtual = Integer.parseInt(formato.format(data));
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ano de nascimento: ");
+        int anoNasc = teclado.nextInt();
+        int idade = anoAtual - anoNasc;
+        System.out.println("A idade e " + idade);
+    }
+    
+}
