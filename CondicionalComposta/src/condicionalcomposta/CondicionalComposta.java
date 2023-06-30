caa678315c6c1adf37607db0d4c49e77aa3dacf9
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-06-30 17:13:14 -0300

    Update CondicionalComposta.java

--- CondicionalComposta/src/condicionalcomposta/CondicionalComposta.java
+++ CondicionalComposta/src/condicionalcomposta/CondicionalComposta.java
@@ -11,10 +11,17 @@ public class CondicionalComposta {
         DateTimeFormatter  formato = DateTimeFormatter.ofPattern("yyyy");
         int anoAtual = Integer.parseInt(formato.format(data));
         Scanner teclado = new Scanner(System.in);
+        
         System.out.print("Ano de nascimento: ");
         int anoNasc = teclado.nextInt();
         int idade = anoAtual - anoNasc;
         System.out.println("A idade e " + idade);
+        
+        if (idade>=18){
+            System.out.println("maior de idade");
+        }else{
+            System.out.println("Menor de idade");
+        }
     }
     
 }
