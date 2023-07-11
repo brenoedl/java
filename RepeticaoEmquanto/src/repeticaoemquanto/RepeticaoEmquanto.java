d18430deba8de2dea8891af610a2c7d1922d4029
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-10 22:50:52 -0300

    Repeticao Emquanto

---
+++ RepeticaoEmquanto/src/repeticaoemquanto/RepeticaoEmquanto.java
@@ -0,0 +1,12 @@
+package repeticaoemquanto;
+
+public class RepeticaoEmquanto {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc<4) {
+            System.out.println("canbalhota " + cc);
+            cc++;
+        }
+    }
+}
