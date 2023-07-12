194a77605e503a94b56b07171e0d562cdcea306c
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-11 22:09:35 -0300

    Repetição Repita

---
+++ RepeticaoRepita/src/repeticaorepita/RepeticaoRepita.java
@@ -0,0 +1,13 @@
+package repeticaorepita;
+
+public class RepeticaoRepita {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        do {            
+            System.out.println("Canbalhota " + cc);
+            cc++;
+        } while (cc < 4);
+    }
+    
+}
