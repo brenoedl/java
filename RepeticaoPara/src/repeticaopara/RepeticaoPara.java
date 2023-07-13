adddf7b90d9b62c193908669f49421331fe0dd8a
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-13 10:26:12 -0300

    Repetição Para

---
+++ RepeticaoPara/src/repeticaopara/RepeticaoPara.java
@@ -0,0 +1,11 @@
+package repeticaopara;
+
+public class RepeticaoPara {
+
+    public static void main(String[] args) {
+        for (int cc = 0; cc <= 3; cc++) {
+            System.out.println("canbalhotta " + cc);
+        }
+    }
+    
+}
