676e95a81de97190f592cc70775d0f5c66c59612
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-15 14:38:27 -0300

    Vetores 03

---
+++ Vetores03/src/vetores03/Vetores03.java
@@ -0,0 +1,12 @@
+package vetores03;
+
+public class Vetores03 {
+
+    public static void main(String[] args) {
+        int n[] = {3, 1, 2, 8, 10, 16, 71, 6, 0};
+        for (int i : n) {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
