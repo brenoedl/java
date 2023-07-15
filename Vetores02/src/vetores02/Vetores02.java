1a41ed168ad930c01bdf77ce01511948cd68ab9b
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-14 21:08:40 -0300

    Vetores 02

---
+++ Vetores02/src/vetores02/Vetores02.java
@@ -0,0 +1,12 @@
+package vetores02;
+
+public class Vetores02 {
+
+    public static void main(String[] args) {
+        int n[] = {5, 30, 8, 3};
+        for (int c = 0; c < n.length; c++) {
+            System.out.println("Na posicaoo " + c + " temos o valor " + n[c]);
+        }
+    }
+    
+}
