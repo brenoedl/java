887fd23447507251b516b80f0a491feee1e9105e
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-16 20:57:09 -0300

    Função 02

---
+++ Funcao02/src/funcao02/Funcao02.java
@@ -0,0 +1,10 @@
+package funcao02;
+
+public class Funcao02 {
+
+    public static void main(String[] args) {
+        String c1 = Operacos.contador(5);
+        System.out.println(c1);
+    }
+    
+}
---
+++ Funcao02/src/funcao02/Operacos.java
@@ -0,0 +1,11 @@
+package funcao02;
+
+public class Operacos {
+    public static String contador(int v){
+        String s ="";
+        for (int c = 0; c <= v; c++) {
+            s += c + " ";
+        }
+        return s;
+    }
+}
