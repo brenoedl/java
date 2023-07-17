30dadbbde065fbba78a7ffebbbf231bf666ca1fb
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-07-17 15:01:26 -0300

    Fatorial Função

---
+++ FatorialFuncao/src/fatorialfuncao/FatorialFuncao.java
@@ -0,0 +1,13 @@
+package fatorialfuncao;
+
+
+public class FatorialFuncao {
+
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+        f.getValor(5);
+        System.out.print(f.setFormola());
+        System.out.println(f.setFatorial());
+    }
+    
+}
